42149ba62f7bd771023d5980e16a9dc1cff750a0
thesstefan <stefanstefanache@protonmail.com>
2024-08-01 22:31:52 +0200

    Removed GitHub PAT (expired) from init_vast.sh

--- init_vast.sh
+++ init_vast.sh
@@ -1,9 +1,10 @@
+GIT_PAT="FILL_WITH_GIT_PAT" 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get -y update 
 sudo apt install -y python3.10 python3.10-venv psmisc neovim unzip git
 python3.10 -m venv env
 . env/bin/activate
-rm -rf road_segmentation && git clone https://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@github.com/thesstefan/road_segmentation.git
+rm -rf road_segmentation && git clone "https://{GIT_PATH}@github.com/thesstefan/road_segmentation.git"
 cd road_segmentation && pip install --no-input ".[dev]"
 cd ..
 mkdir -p data && cd data
